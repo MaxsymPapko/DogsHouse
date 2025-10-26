bcb510fef36c6f5a94a1337564af178d47998294
Maxsym Papko <papkom50@gmail.com>
2025-10-27 00:27:00 +0200

    create a DB in MSSQL Server, fix Connection String and Login issues

--- Program.cs
+++ Program.cs
@@ -1,15 +1,32 @@
 using DogsHouseService.Data;
 using Microsoft.EntityFrameworkCore;
+using AspNetCoreRateLimit;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(options => 
+{
+    options.GeneralRules = new List<RateLimitRule>
+    {
+        new RateLimitRule
+        {
+            Endpoint ="*",
+            Limit =10,
+            Period = "1s"
+        }
+    };
+});
+
+builder.Services.AddInMemoryRateLimiting();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
 
 builder.Services.AddDbContext<DogsContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -17,6 +34,8 @@ app.MapControllers();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseIpRateLimiting();
 
 
 app.MapGet("/", ()=> "Server is works!");
